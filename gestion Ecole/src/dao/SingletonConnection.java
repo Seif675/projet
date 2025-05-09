d11117b4de00e96a0acc039ce8dd19d204caaaad
nesrine200421 <nesrine.fekihhassen@isimg.tn>
2025-05-09 20:49:32 +0200

    Update SingletonConnection.java

--- gestion Ecole/src/dao/SingletonConnection.java
+++ gestion Ecole/src/dao/SingletonConnection.java
@@ -1,46 +1,37 @@
 package dao;
-import java.io.FileInputStream;
+
 import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
 public class SingletonConnection {
-Properties props=new Properties();
-private static String user;
-private static String password;
-private static String url;
-//Objet Connection
-private static Connection connect;
-//Constructeur privé
-private SingletonConnection(){
-try {
-	
-	props.load(new FileInputStream("conf.properties"));
-	url=props.getProperty("jdbc.url");
-	user=props.getProperty("jdbc.user");
-	password=props.getProperty("jdbc.password");
-	connect = DriverManager.getConnection(url, user, password);
-     //System.out.println("connecte");
-} 
-catch (SQLException e)
-{ e.printStackTrace();; 
-}
-catch(IOException e)
-{
-	e.printStackTrace();
-}
-}
-//Méthode qui retourne l’instance et la créer si elle n'existe pas
-public static Connection getInstance(){
-if(connect == null){
-	
-new SingletonConnection();
-}
-return connect;
-}
+    private static Connection connect;
+    private static Properties props = new Properties();
 
+    static {
+        try (InputStream input = SingletonConnection.class.getClassLoader().getResourceAsStream("dao/conf.properties")) {
+            props.load(input);
+            String url = props.getProperty("jdbc.url");
+            String user = props.getProperty("jdbc.user");
+            String password = props.getProperty("jdbc.password");
 
+            connect = DriverManager.getConnection(url, user, password);
+            connect.setAutoCommit(true);
+        } catch (IOException | SQLException e) {
+            throw new ExceptionInInitializerError("Erreur d'initialisation: " + e.getMessage());
+        }
+    }
 
+    public static synchronized Connection getInstance() throws SQLException {
+        if (connect == null || connect.isClosed()) {
+            String url = props.getProperty("jdbc.url");
+            String user = props.getProperty("jdbc.user");
+            String password = props.getProperty("jdbc.password");
+            connect = DriverManager.getConnection(url, user, password);
+        }
+        return connect;
+    }
 }
